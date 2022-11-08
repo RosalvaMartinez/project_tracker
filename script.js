ea9bc6281623fea02b3d216837b8a69e770ab3fe
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2022-11-08 12:58:42 -0600

    Add table input

--- script.js
+++ script.js
@@ -11,4 +11,25 @@ $( function() {
     $( "#datepicker" ).datepicker();
   } );
 
+  $("#button").on("click", addProject)
+  var projects = []
+  function addProject () {
+    //get user input from text input fields
+   var name = $("#input").val()
+   var type = $("#inputGroupSelect02")
+   //create object to store project name and type
+   var ptoject = {
+       name: name,
+       type: type
+  }
+   //push project objects into global array of projects
+   projects.push(project);
+   //convert array of project objects into string first then save in local storage
+   localStorage.setItem("project", JSON.stringify(projects))
 
+   projects = JSON.parse(localStorage.getItem("project"));
+}
+  
+
+
+   
