bcbc0425bf38523ad0da51aff77af7a74c63265a
Alejandro Zapien <alejandro@satxconsultants.com>
2022-11-07 21:34:12 -0600

    Add Clock in Hero

---
+++ script.js
@@ -0,0 +1,7 @@
+function startClock () {
+    var ClockInterval = setInterval(function () {
+        $('#clock').children('h1').text(dayjs().format("dddd, MMMM D h:mm:ss"))
+    }, 1000)
+}
+
+startClock()
